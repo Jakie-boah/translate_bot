325b53cc7a59232f50c9f5a236de5e463d167ae2
NAME <VanoGalen@yandex.ru>
2023-05-17 00:32:22 +0300

    main

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ from handlers.album_handler import AlbumMiddleware
 if __name__ == '__main__':
     dp.middleware.setup(AlbumMiddleware())
 
-    logger.info("Бот запущен")
+    logger.info("Бот запущен ветка new_version")
     # dp.start_polling(bot)
     executor.start_polling(dp, skip_updates=True)
